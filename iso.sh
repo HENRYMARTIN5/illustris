643e4d9fa80d89d076fa54f02ceb5f9b58a6145c
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-03-25 17:56:35 -0500

    Rename everything from myos to solus

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/solus.kernel isodir/boot/solus.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "solus" {
+	multiboot /boot/solus.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o solus.iso isodir
