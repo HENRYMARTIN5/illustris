73686824d7688a86a269479700afc62e3c9ac32e
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-03-27 11:07:53 -0500

    Update kernel.c

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -6,6 +6,10 @@ void test_task() {
 	printf("Hello, world!\n");
 }
 
+void test_task_2() {
+	printf("Hello, second process world!\n");
+}
+
 void kernel_main(void) {
 	terminal_initialize();
 	printf("Creating root proc...");
@@ -14,8 +18,23 @@ void kernel_main(void) {
 	printf("Creating task...");
 	int tid = task_create(pid, &test_task);
 	printf("done\n");
+	
+	printf("Creating second proc...");
+	int pid2 = proc_create();
+	printf("done\n");
+	printf("Creating task...");
+	int tid2 = task_create(pid2, &test_task_2);
+	printf("done\n");
+	
+	printf("Forcenext-ing second proc...");
+	proc_forcenext(pid2);
+	printf("done\n");
+	
 	printf("Starting main loop...\n");
 	while (1) {
 		proc_tick();
+		// Expected output:
+		// Hello, second process world!
+		// Hello, world!
 	}
 }
