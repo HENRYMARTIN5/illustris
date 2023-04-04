29ae6134e4e0e837c42ad47366b2f21d863db096
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-04-04 10:44:03 -0500

    Experimental VGA output

--- kernel/main.rs
+++ kernel/main.rs
@@ -2,7 +2,7 @@
 #![no_std]	//< Kernels can't use std
 #![crate_name="kernel"]
 
-/// Macros, need to be loaded before everything else due to how rust parses
+// Macros, need to be loaded before everything else due to how rust parses
 #[macro_use]
 mod macros;
 
@@ -12,17 +12,23 @@ pub mod arch;
 #[cfg(target_arch="x86")] #[path="arch/x86/mod.rs"]
 pub mod arch;
 
-/// Exception handling (panic)
+// Exception handling (panic)
 pub mod unwind;
 
-/// Logging code
+// Logging code
 mod logging;
+
+use vga::{Mode, FrameBuffer};
 
 // Kernel entrypoint (called by arch/<foo>/start.S)
 #[no_mangle]
 pub fn kmain()
-{
-	log!("Hello world! 1={}", 1);
-	loop {}
+{	
+	// Initialize vga
+	let mode = Mode::new();
+    let fb = FrameBuffer::new(mode);
+
+	fb.writer().write_str(":O wow rust is way easier than c for osdev").unwrap();
+	loop {} // If we exit, the machine shuts down - we don't want that, so we loop forever
 }
 
