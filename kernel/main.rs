30b3f95723c243f14df59fa2c72b2dcfb2e8d4f0
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-04-04 10:52:32 -0500

    Fix VGA test

--- kernel/main.rs
+++ kernel/main.rs
@@ -19,17 +19,24 @@ pub mod unwind;
 mod logging;
 
 extern crate vga;
-use vga::{Mode, FrameBuffer};
+use vga::colors::Color16;
+use vga::writers::{Graphics640x480x16, GraphicsWriter};
 
 // Kernel entrypoint (called by arch/<foo>/start.S)
 #[no_mangle]
 pub fn kmain()
 {	
-	// Initialize vga
-	let mode = Mode::new();
-    let fb = FrameBuffer::new(mode);
-
-	fb.writer().write_str(":O wow rust is way easier than c for osdev").unwrap();
+	let mode = Graphics640x480x16::new();
+	mode.set_mode();
+	mode.clear_screen(Color16::Black);
+	mode.draw_line((80, 60), (80, 420), Color16::White);
+	mode.draw_line((80, 60), (540, 60), Color16::White);
+	mode.draw_line((80, 420), (540, 420), Color16::White);
+	mode.draw_line((540, 420), (540, 60), Color16::White);
+	mode.draw_line((80, 90), (540, 90), Color16::White);
+	for (offset, character) in "Hello World!".chars().enumerate() {
+		mode.draw_character(270 + offset * 8, 72, character, Color16::White)
+	}
 	loop {} // If we exit, the machine shuts down - we don't want that, so we loop forever
 }
 
