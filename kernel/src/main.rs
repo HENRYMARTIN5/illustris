79107d1fc8cdac4d09d96db6bb2ed25b8f1f880e
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2023-04-13 17:29:41 -0500

    Add logging

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -15,17 +15,26 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use illustris::memory::{self, BootInfoFrameAllocator};
     use x86_64::VirtAddr;
 
-    println!("Hello World{}", "!");
+    println!("----- Welcome to Illustris! -----");
+    print!("Initializing core...");
     illustris::init();
+    println!("done.");
 
+    print!("Initializing memory...");
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    println!("done.");
 
-    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+    print!("Initializing heap...");
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("Heap initialization failed!");
+    println!("done.");
 
+    println!("Initializing executor...");
     let mut executor = Executor::new();
-    executor.spawn(Task::new(example_task()));
+    println!("done.");
+
+    println!("Spawning keyboard task...");
     executor.spawn(Task::new(keyboard::print_keypresses()));
     executor.run();
 }
@@ -37,11 +46,11 @@ fn panic(info: &PanicInfo) -> ! {
     illustris::hlt_loop();
 }
 
-async fn async_number() -> u32 {
-    42
-}
+// async fn async_number() -> u32 {
+//     42
+// }
 
-async fn example_task() {
-    let number = async_number().await;
-    println!("async number: {}", number);
-}
+// async fn example_task() {
+//     let number = async_number().await;
+//     println!("async number: {}", number);
+// }
